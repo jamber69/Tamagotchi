4c5700da02a5513c82469a4fed146fdfba22d5fb
jamber69 <jack.sandelin@elev.ga.ntig.se>
2024-01-12 12:51:07 +0100

    1

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Game {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Game {
+    ArrayList<String> words = new ArrayList<String>();
+
+    public Game() {
+        start();
+    }
+
+    private void start() {
+        Tamagotchi mytamagotchi = new Tamagotchi();
+        boolean alive = true;
+        while (alive) {
+            System.out.println("Hej! Här är din tamagotchi");
+            System.out.println(" ");
+            System.out.println("1: Lär din tamagotchi ett nytt ord");
+            System.out.println("2: Din tamagotchi säger ett ord");
+            System.out.println("3: Vill du mata din tamagotchi");
+            System.out.println("4. Vill du se din tamagotchis stats");
+
+
+            System.out.println("Tryck k om du vill döda din tamagotchi");
+
+
+            Scanner myScanner = new Scanner(System.in);
+            boolean bool = false;
+            String yes = myScanner.nextLine();
+            if (yes.equals("1")) {
+                mytamagotchi.Teach();
+            }
+            if (yes.equals("2")) {
+                mytamagotchi.Hi();
+            }
+            if (yes.equals("3")) {
+                mytamagotchi.Feed();
+            }
+            if (yes.equals("4")) {
+                mytamagotchi.PrintStats();
+            }
+
+            if (yes.equals("k")) {
+                alive = false;
+            }
+
+        }
+        System.out.println("Din tamagotchi är död :(");
+    }
+
+
 }
--- src/Tamagotchi.java
+++ src/Tamagotchi.java
@@ -1,2 +1,81 @@
-package PACKAGE_NAME;public class Tamagotchi {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Tamagotchi {
+    //Attributes
+    private int hunger;
+    private int boredom = 0;
+    private ArrayList<String> words = new ArrayList<String>();
+    private boolean isAlive;
+    private Random generator = new Random();
+    Scanner wordScanner = new Scanner(System.in);
+    public String name;
+
+
+    //Constructers
+    Tamagotchi() {
+
+    }
+
+    //Methods
+    public void Feed() {
+
+    }
+
+    public void Hi() {
+        //   Collections.shuffle(words);
+        while (true) {
+
+            if (words.size() > 0) {
+                System.out.println(words.get(generator.nextInt(words.size())));
+            } else {
+                System.out.println("gagabogo!");
+            }
+
+            System.out.println("Vill du att din tamagotchi säger ett annat ord? y/n");
+            String no = wordScanner.nextLine();
+            if (no.equals("n")) {
+                break;
+            }
+        }
+
+    }
+
+    public void Teach() {
+
+        while (true) {
+            System.out.println("Vilket nytt ord vill du lära din tamagotchi?");
+
+            String newWord = wordScanner.nextLine();
+            words.add(newWord);
+
+            System.out.println(words);
+
+            System.out.println("Vill du lära tamagotchin ett till ord? y/n");
+            String no = wordScanner.nextLine();
+            if (no.equals("n")) {
+                break;
+            }
+        }
+
+
+    }
+
+    public void Tick() {
+
+    }
+
+    public void PrintStats() {
+        System.out.println(hunger);
+        System.out.println(boredom);
+    }
+
+    private void ReduceBoredom() {
+
+    }
+
+    // Setters & getters
+
 }
